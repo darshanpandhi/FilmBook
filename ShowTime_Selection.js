02ec225f240ac2fbf4f4e56ef06f9a08311116a0
ZuneNebula <andromedainvader001@gmail.com>
2019-11-18 13:09:06 -0600

    Add files via upload

---
+++ ShowTime_Selection.js
@@ -0,0 +1,23 @@
+function openPopUp(){
+    document.getElementById("popUp").style.display = "block";
+    document.getElementById("open").style.display = "none";
+}
+function closePopUp(){
+    document.getElementById("popUp").style.display = "none";
+    document.getElementById("open").style.display = "block";
+}
+
+displayDate()
+{
+    document.getElementById("date").style.display = '';
+}
+
+displayShowtime()
+{
+    document.getElementById("showtime").style.display ='';
+}
+
+displayConfirm()
+{
+    document.getElementById("confirm").style.display = '';
+}
