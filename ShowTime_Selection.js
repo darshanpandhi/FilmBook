aa36111a40868040ec0469df01efa184709f78a7
ZuneNebula <andromedainvader001@gmail.com>
2019-11-22 08:36:25 -0600

    Add files via upload

--- ShowTime_Selection.js
+++ ShowTime_Selection.js
@@ -7,17 +7,18 @@ function closePopUp(){
     document.getElementById("open").style.display = "block";
 }
 
+
 displayDate()
 {
-    document.getElementById("date").style.display = '';
+    document.getElementById("date").style.display ="block";
 }
 
 displayShowtime()
 {
-    document.getElementById("showtime").style.display ='';
+    document.getElementById("showtime").style.display ="block";
 }
 
 displayConfirm()
 {
-    document.getElementById("confirm").style.display = '';
+    document.getElementById("confirm").style.display = "block";
 }
