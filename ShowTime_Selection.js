06fe0782b602130d711ea6a8177d2ff1db071534
ZuneNebula <andromedainvader001@gmail.com>
2019-11-23 17:21:10 -0600

    added showtime-selection

---
+++ ShowTime_Selection.js
@@ -0,0 +1,24 @@
+function openPopUp(){
+    document.getElementById("popUp").style.display = "block";
+    document.getElementById("open").style.display = "none";
+}
+function closePopUp(){
+    document.getElementById("popUp").style.display = "none";
+    document.getElementById("open").style.display = "block";
+}
+
+
+function displayDate()
+{
+    document.getElementById("date").style.display ="block";
+}
+
+function displayShowtime()
+{
+    document.getElementById("showtime").style.display ="block";
+}
+
+function displayConfirm()
+{
+    document.getElementById("confirm").style.display = "block";
+}
